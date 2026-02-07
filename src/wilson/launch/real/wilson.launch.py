0068446ecbaa41f202c5929aa502c7bb4a09139e
tracelarue <traceglarue@gmail.com>
2026-02-07 15:10:18 -0600

    wilson launch update

--- src/wilson/launch/real/wilson.launch.py
+++ src/wilson/launch/real/wilson.launch.py
@@ -218,7 +218,7 @@ def generate_launch_description():
     )
 
     gemini_ros_mcp = ExecuteProcess(
-    cmd=['tilix', '-e', 'bash', '-c', 'python3 og.py --mode=robot; echo "Process finished. Press enter to close..."; read'],
+    cmd=['tilix', '-e', 'bash', '-c', 'python3 gemini.py --mode=robot; echo "Process finished. Press enter to close..."; read'],
     cwd=gemini_mcp_path,
     output='screen',
     )
--- src/wilson/launch/sim/wilson_sim.launch.py
+++ src/wilson/launch/sim/wilson_sim.launch.py
@@ -247,7 +247,7 @@ def generate_launch_description():
     )
 
     gemini_ros_mcp = ExecuteProcess(
-        cmd=['tilix', '-e', 'bash', '-c', 'python3 og.py --responses="AUDIO"; echo "\n\nScript exited. Press Enter to close..."; read'],
+        cmd=['tilix', '-e', 'bash', '-c', 'python3 gemini.py --responses="AUDIO"; echo "\n\nScript exited. Press Enter to close..."; read'],
         cwd=gemini_mcp_path,
         output='screen',
     )
